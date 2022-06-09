e363b5412d57fff4ca3c37011df0d37d78593ec2
saacostam <saacostam@unal.edu.co>
2022-06-08 22:23:17 -0500

    request

--- request.js
+++ request.js
@@ -1,5 +1,6 @@
 // const { default: request } = require('graphql-request');
 const soap = require('soap');
+const axios = require('axios');
 
 // const url = 'http://34.123.106.254:3009/wsdl?wsdl';
 const url = "https://api.skillsly.app/soap/ws"
@@ -15,25 +16,60 @@ async function requestFunc(req, res) {
     //     }
     // })
 
-    soap.createClient(url, function(err, client) {
-        if (err) {
-            throw err;
-        }
-        /* 
-         * Parameters of the service call: they need to be called as specified
-         * in the WSDL file
-         */
-        var args = {
-            userId: id
-        };
-        // call the service
-        client.getUser(args, function(err, response) {
-            if (err)
-                throw err;
-            // print the service returned result
+    let xmls = `<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:gs="http://spring.io/guides/gs-producing-web-service">\
+    <soapenv:Header/>\
+    <soapenv:Body>\
+       <gs:getUserRequest>\
+          <gs:userId>${id}</gs:userId>\
+       </gs:getUserRequest>\
+    </soapenv:Body>\
+ </soapenv:Envelope>`;
+
+    axios.post(url,
+            xmls, {
+                headers: { 'Content-Type': 'text/xml' }
+            })
+        .then(data => {
+            let dataArray = data.data.split('<ns2:id>');
+            dataArray = dataArray[1].split('</ns2:id>');
+            const id = dataArray[0];
+            dataArray = dataArray[1].split('<ns2:name>');
+            dataArray = dataArray[1].split('</ns2:name>');
+            const user = dataArray[0];
+            dataArray = dataArray[1].split('</ns2:email>');
+            const email = dataArray[0].split('<ns2:email>')[1];
+            console.log(id, user, email);
+
+            const response = {
+                user: {
+                    id: id,
+                    user: user,
+                    email: email
+                }
+            }
             return res.json(response);
-        });
-    });
+        })
+        .catch(err => { console.log(err) });
+
+    // soap.createClient(url, function(err, client) {
+    //     if (err) {
+    //         throw err;
+    //     }
+    //     /* 
+    //      * Parameters of the service call: they need to be called as specified
+    //      * in the WSDL file
+    //      */
+    //     var args = {
+    //         userId: id
+    //     };
+    //     // call the service
+    //     client.getUser(args, function(err, response) {
+    //         if (err)
+    //             throw err;
+    //         // print the service returned result
+    //         return res.json(response);
+    //     });
+    // });
 };
 
 module.exports = requestFunc;
