2f061f36aaf0b293e637a736f7820ba079c3e383
saacostam <saacostam@unal.edu.co>
2022-06-27 21:42:27 -0500

    fix-uri

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ import { request, gql } from 'graphql-request'
 
 import requestFunc from './request.js'
 
-const url = "https://35.226.199.77.nip.io/graphql";
+const url = "http://35.223.151.61.nip.io/graphql";
 
 function getQueryForId(id) {
     const query = gql `{
